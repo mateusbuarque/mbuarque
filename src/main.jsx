aa636de5735a2b333b8bcafa5ed567ad55fb002e
mateusbuarque <mateusbpugli@gmail.com>
2026-08-11 12:13:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1236,6 +1236,7 @@ function Cart({ data, cart, customer, notify }) {
                   : foundCoupon
                     ? couponResult.message
                     : "Cupom não encontrado"}
+                  <button onClick={() => >Retirada</button>
               </small>
             )}
 
